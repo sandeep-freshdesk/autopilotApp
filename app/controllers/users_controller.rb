b2e7b3088a27a5b020390685793a458c27efe292
sandeep-freshdesk <sandeep@freshdesk.com>
2017-02-09 21:57:11 +0530

    signup functionality completed end of chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
 	def new
 		@title = "sign up"
+		@user = User.new
 	end
 
 	def show
 		@user = User.find(params[:id])
+		@title = @user.name
+	end
+
+	def create
+		@user = User.new(params[:user])
+
+		if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			puts "sfdsdf \n sdfsdfs \n sdfsdf\n sdf\n sfsdf\n sdfsd\n \nsfdsdf\nsdfsdf"
+			redirect_to @user
+		else 
+			@title = 'Sign Up'
+			render 'new'
+		end
 		
 	end
+
+	private 
+		def user_params
+			params(:user).permit(:name, :email, :password, :password_confirmation)
+		end
 end
