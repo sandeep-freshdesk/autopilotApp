fb2b42baef5c2073cd02e08e525c8a768851be06
sandeep-freshdesk <sandeep@freshdesk.com>
2017-02-10 19:00:19 +0530

    autopilot resclient calling

--- app/models/autopilot_model.rb
+++ app/models/autopilot_model.rb
@@ -2,12 +2,11 @@ class AutopilotModel < ActiveRecord::Base
 	require 'rest_client'
 
 	@url = "https://api2.autopilothq.com/v1/contact"
-	@autopilotapiKey = "d1be3465d4a74719b2dd6c27faf8c884"
-	@contentType = "application/json"
 
 	def self.getData
 		puts "get data"
-		@response = RestClient.get(@url, { :"Content-Type" => "json" , "autopilotapikey" => "d1be3465d4a74719b2dd6c27faf8c884" }	 )
+		debugger
+		@response = RestClient.get(@url,{:content_Type => "application/json", "autopilotapikey" => "d1be3465d4a74719b2dd6c27faf8c884"})
 		puts "end of "
 	end
 
